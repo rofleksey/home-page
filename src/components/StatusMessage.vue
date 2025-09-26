796ee35292880fb17f728d2970264bbe1ffd5a7e
rofleksey <evilmorty@yandex.ru>
2025-09-26 15:42:35 +0300

    Update demo

--- src/components/StatusMessage.vue
+++ src/components/StatusMessage.vue
@@ -1,10 +1,14 @@
 <template>
-  <p class="status">// Currently brewing coffee and debugging half-finished services</p>
+  <p class="status">
+    // Currently brewing coffee and debugging half-finished services <br/>
+    // This page is unfinished as well...
+  </p>
 </template>
 
 <style scoped>
 .status {
   font-size: 0.8rem;
   color: #666;
+  text-align: left;
 }
 </style>
